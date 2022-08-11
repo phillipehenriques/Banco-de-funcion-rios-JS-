a7b7f9f9e5e2d90f5969cbcb9c58e66ccad83b56
phillipe henriques <phillipe_ch@hotmail.com>
2022-08-11 00:11:18 -0300

    todos os valores em string funcionando com evento, menos os valores numericos

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,38 +1,92 @@
 
 let Patricia = {
-    foto: 'imagens/patricia.png',
+    id: 1,
+    imagem: 'imagens/patricia.png',
     nome: 'Patricia',
-    sexo: 'F', 
-    setor: 'RH' ,
-    tempo: '3 anos e 4 meses'
+    sexo: 'Feminino', 
+    cargo: 'Analista de recursos humanos',
+    setor: 'RH',
+    contratacao: '06.08.2022',
+    desligamento: 'x',
+    tempo: '01 dia(s)',
 }
 
 let Rafael = {
+    id: 2,
+    imagem: 'imagens/rafael.png',
     nome: 'Rafael',
-    sexo: 'M', 
-    setor: 'Help Desk' ,
-    tempo: '9 meses'
+    sexo: 'Masculino',
+    cargo: 'Analista de rede', 
+    setor: 'Help Desk',
+    contratacao: '06.08.2022',
+    desligamento: 'x',
+    tempo: '01 dia(s)',
 }
 
 let Carol = {
+    id: 3,
+    imagem: 'imagens/carol.png',
     nome: 'Carol',
-    sexo: 'F', 
-    setor: 'UI/UX Designer' ,
-    tempo: '1 ano e 5 meses'
+    sexo: 'Feminino',
+    cargo: 'Designer', 
+    setor: 'UI/UX Designer',
+    contratacao: '06.08.2022',
+    desligamento: 'x',
+    tempo: '01 dia(s)',
 }
 
-let funcionarios = [Patricia, Rafael, Carol]
+let Rodrigo = {
+    id: 4,
+    imagem: 'imagens/rodrigo.png',
+    nome: 'Rodrigo',
+    sexo: 'Masculino',
+    cargo: 'Vendedor', 
+    setor: 'Vendas',
+    contratacao: '06.08.2022',
+    desligamento: 'x',
+    tempo: '01 dia(s)',
+}
+
+let Daniel = {
+    id: 5,
+    imagem: 'imagens/daniel.png',
+    nome: 'Daniel',
+    sexo: 'Masculino',
+    cargo: 'SEO',
+    setor: 'Gerencia',
+    contratacao: '06.08.2022',
+    desligamento: 'x',
+    tempo: '01 dia(s)',
+}
+
+let funcionarios = [Patricia, Rafael, Carol, Rodrigo, Daniel]
 
 let select = document.querySelector('select')
 funcionarios.forEach((element) => {
-    select.options.add(new Option(element.nome, element.name))
+    select.options.add(new Option(element.nome, element.id))
 })
 
-console.log(funcionarios[4].nome)
+function changeEmployee(changeValue) {
+    
+    let employee = funcionarios.find(x => x.id == changeValue)
+    
+    // alert(typeof(changeValue)) <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
 
-function selecionar() {
-    let funcionarios = document.getElementById('patricia')
-    if (funcionarios != null) {
-        funcionarios.innerHTML = '<img class="#" src="imagens/'+ id +'.jpg" alt=""/>'
-      }
+    // let employee = null
+    // for (var index=0; index <= funcionarios.length; index++) {
+    //     if (funcionarios[index].id === changeValue) {
+    //         employee = funcionarios[index]
+    //     } 
+    // }
+
+    let nome = document.getElementById('name').innerHTML = employee.nome
+    let sexo = document.getElementById('sex').innerHTML = employee.sexo
+    let cargo = document.getElementById('position').innerHTML = employee.cargo
+    let setor = document.getElementById('sector').innerHTML = employee.setor
+    let contratacao = document.getElementById('hiring-date') = employee.contratacao
+    let desligamento = document.getElementById('risignation-date') = employee.desligamento
+    let tempo = document.getElementById('total-time') = employee.tempo
+
+ // selecionar na lista funcionarios qual é o funcionário cujo Id é igual ao id que estou recebendo no changeValue.
+ // pegar todas as informações desse funcionario selecionado e adicionar ao innerHTML de cada um dos campos na tela.
 }
